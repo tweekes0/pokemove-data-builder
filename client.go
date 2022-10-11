c9f301e71bc26d1b6a55b36b260e39049d2aba65
Tweekes <tarwee@protonmail.com>
2022-10-11 14:36:02 -0400

    feat: add struct to get pokemon from endpoint

--- client.go
+++ client.go
@@ -25,22 +25,22 @@ type flavorText struct {
 // struct for pokeapi pastMoveValues
 // https://pokeapi.co/docs/v2#moves
 type pastMoveValue struct {
-	Accuracy      int             `json:"accuracy"`
-	EffectChance  int             `json:"effect_chance"`
-	Power         int             `json:"power"`
-	PowerPoints   int             `json:"pp"`
-	Type          namedResource   `json:"type"`
-	VersionGroup  namedResource   `json:"version_group"`
+	Accuracy     int           `json:"accuracy"`
+	EffectChance int           `json:"effect_chance"`
+	Power        int           `json:"power"`
+	PowerPoints  int           `json:"pp"`
+	Type         namedResource `json:"type"`
+	VersionGroup namedResource `json:"version_group"`
 }
 
 // struct for pokeapi endpoints when not supplied with
 // a parameter
-type BasicResponse struct {
+type basicResponse struct {
 	Count   int             `json:"count"`
 	Results []namedResource `json:"results"`
 }
 
-func (r *BasicResponse) Print() {
+func (r *basicResponse) Print() {
 	fmt.Printf("Count: %v\n", r.Count)
 	fmt.Println("Results:")
 	for _, r := range r.Results {
@@ -63,14 +63,34 @@ type MoveResponse struct {
 	PastValues  []pastMoveValue `json:"past_values"`
 }
 
-func (r *MoveResponse) Print() {
-	fmt.Printf("Move ID: %v\n", r.ID)
-	fmt.Printf("Move Accuracy: %v\n", r.Accuracy)
-	fmt.Printf("Move Power: %v\n", r.Power)
-	fmt.Printf("Move Name: %v\n", r.Name)
-	fmt.Printf("Move Type: %v\n", r.Type)
-	fmt.Printf("Move Generation: %v\n", r.Generation)
-	fmt.Println()
+type pokemonAbility struct {
+	Hidden  bool          `json:"is_hidden"`
+	Slot    int           `json:"slot"`
+	Ability namedResource `json:"ability"`
+}
+
+type pokemonSprite struct {
+	FrontDefault string                   `json:"front_default"`
+	Other        map[string]pokemonSprite `json:"other"`
+}
+
+type versionGroupDetails struct {
+	LearnedLevel  int           `json:"level_learned_at"`
+	LearnedMethod namedResource `json:"move_learn_method"`
+	VersionGroup  namedResource `json:"version_group"`
+}
+
+type move struct {
+	Name    namedResource         `json:"move"`
+	Details []versionGroupDetails `json:"version_group_details"`
+}
+
+type PokemonResponse struct {
+	ID        int              `json:"id"`
+	Name      string           `json:"name"`
+	Abilities []pokemonAbility `json:"abilities"`
+	Sprite    pokemonSprite    `json:"sprites"`
+	Moves     []move           `json:"moves"`
 }
 
 func getResponse(url string) ([]byte, error) {
@@ -90,8 +110,8 @@ func getResponse(url string) ([]byte, error) {
 	return data, nil
 }
 
-func getBasicMoveResponse(limit int, endpoint string) (BasicResponse, error) {
-	var basicResp BasicResponse
+func getBasicMoveResponse(limit int, endpoint string) (basicResponse, error) {
+	var basicResp basicResponse
 
 	url := fmt.Sprintf("%v?limit=%v", endpoint, limit)
 
