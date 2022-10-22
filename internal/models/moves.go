f02ff4c7ebc57e2f898916920e62d840d76cb682
Tweekes <tarwee@protonmail.com>
2022-10-21 22:09:46 -0400

    refactor/fix: fix sql typo and update pokemon move get func

--- internal/models/moves.go
+++ internal/models/moves.go
@@ -3,6 +3,7 @@ package models
 import (
 	"database/sql"
 	"errors"
+	// "errors"
 
 	"github.com/tweekes0/pokemonmoves-backend/internal/client"
 )
@@ -10,14 +11,15 @@ import (
 const (
 	moveInsert = `INSERT INTO pokemon_moves(move_id, name, accuracy, power,
 		powerpoints, generation, type, damagetype, description) 
-	VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`
+		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`
 	moveDelete  = `DELETE FROM pokemon_moves WHERE move_id = $1`
 	moveGetByID = `SELECT move_id, name, accuracy, power, powerpoints,
-	generation, type, damagetype, description FROM pokemon_moves WHERE poke_id = $1`
+		generation, type, damagetype, description 
+		FROM pokemon_moves WHERE move_id = $1 AND generation = $2`
 	moveGetByName = `SELECT move_id, name, accuracy, power, powerpoints,
-	generation, type, damagetype, description FROM pokemon_moves WHERE name = $1`
+		generation, type, damagetype, description FROM pokemon_moves WHERE name = $1`
 	moveGetAll = `SELECT move_id, name, accuracy, power, powerpoints,
-	generation, type, damagetype, description FROM pokemon_moves`
+		generation, type, damagetype, description FROM pokemon_moves`
 	moveExists = `SELECT EXISTS(SELECT 1 FROM pokemon_moves WHERE id = $1)`
 )
 
@@ -39,14 +41,13 @@ func (m *MovesModel) MoveInsert(mv client.PokemonMove) error {
 	return nil
 }
 
-func (m *MovesModel) MoveGet(moveID int) (*client.PokemonMove, error) {
+func (m *MovesModel) MoveGet(moveID, gen int) (*client.PokemonMove, error) {
 	mv := &client.PokemonMove{}
 
-	err := m.DB.QueryRow(pokemonGetByID, moveID).Scan(
+	err := m.DB.QueryRow(moveGetByID, moveID, gen).Scan(
 		&mv.MoveID, &mv.Name, &mv.Accuracy, &mv.Power, &mv.PowerPoints,
 		&mv.Generation, &mv.Type, &mv.DamageType, &mv.Description,
 	)
-
 	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			return nil, ErrDoesNotExist
