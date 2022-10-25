8e170a843c7798f3fed4bea68452e0e709d2641f
Tweekes <tarwee@protonmail.com>
2022-10-25 15:53:38 -0400

    feat: add abilities join

--- internal/models/abilities.go
+++ internal/models/abilities.go
@@ -12,11 +12,29 @@ const (
 		description, generation) VALUES ($1, $2, $3, $4)`
 	abilityGetById = `SELECT ability_id, name, description, generation 
 	FROM pokemon_abilities WHERE ability_id = $1`
-	abilityGetAll = `SELECT ability_id, name, description, generation FROM pokemon_abilities`
+	abilityGetAll        = `SELECT ability_id, name, description, generation FROM pokemon_abilities`
+	pokemonAbilitiesJoin = `
+	SELECT DISTINCT
+	pa.ability_id, pa.name, pa.description, pa.generation,
+	par.slot, par.hidden
+	FROM pokemon p
+	JOIN pokemon_ability_rels par on p.poke_id = par.poke_id
+	JOIN pokemon_abilities pa on pa.ability_id = par.ability_id
+	WHERE p.poke_id = $1
+	`
 )
 
 type AbilitiesModel struct {
 	DB *sql.DB
+}
+
+type AbilityJoinRow struct {
+	ID          int
+	Name        string
+	Description string
+	Generation  int
+	Slot        int
+	Hidden      bool
 }
 
 func (m *AbilitiesModel) AbilityInsert(a client.PokemonAbility) error {
@@ -104,16 +122,41 @@ func (m *AbilitiesModel) AbilityRelationsBulkInsert(
 	}
 	stmt, teardown := transactionSetup(m.DB, tblInfo)
 
-	for _, rel := range rels {	
+	for _, rel := range rels {
 		_, err := stmt.Exec(rel.PokeID, rel.AbilityID, rel.Slot, rel.Hidden)
 		if err != nil {
 			return err
 		}
 	}
-	
+
 	if err := teardown(); err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func (m *AbilitiesModel) PokemonAbilitiesJoin(pokeID int) ([]*AbilityJoinRow, error) {
+	abs := []*AbilityJoinRow{}
+
+	rows, err := m.DB.Query(pokemonAbilitiesJoin, pokeID)
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		ab := &AbilityJoinRow{}
+		err := rows.Scan(
+			&ab.ID, &ab.Name, &ab.Description,
+			&ab.Generation, &ab.Slot, &ab.Hidden,
+		)
+
+		if err != nil {
+			return nil, err
+		}
+
+		abs = append(abs, ab)
+	}
+
+	return abs, nil
 }
